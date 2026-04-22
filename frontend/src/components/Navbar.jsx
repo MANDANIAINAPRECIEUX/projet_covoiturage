6e69e78ac9c69a2f9a31897bb21874aee36745ed
unknown <mandaniainaprecieux@yahoo.com>
2026-04-22 23:46:26 +0300

    gop1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           <div className="text-base lg:text-lg font-semibold">SE CONNECTER</div>
         </Link>
 
-        <Link to="/signup">
+        <Link to="/signup"> dfgdgdfs gsdgsdg dfgsdgsdg dfgsdfgdfg dfgsdgfs
           <div className="text-base lg:text-lg font-semibold">S'INSCRIRE</div>
         </Link>
       </div>
