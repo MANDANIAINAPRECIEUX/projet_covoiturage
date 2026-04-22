ec14762ec9c778c1c6a769d3d65716d063583816
unknown <mandaniainaprecieux@yahoo.com>
2026-04-22 23:43:48 +0300

    glo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         </Link>
 
         <Link to="/signup">
-          <div className="text-base lg:text-lg font-semibold">S'INSCRIRE</div>gergereg sdfsdf sdfsdfsdf sdfsdfsdf
+          <div className="text-base lg:text-lg font-semibold">S'INSCRIRE</div>gergereg sdfsdf sdfsdfsdf
         </Link>
       </div>
 
