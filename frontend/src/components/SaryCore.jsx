2fb6347f5528852473b50c94225861f09229e55b
unknown <mandaniainaprecieux@yahoo.com>
2026-04-23 23:09:20 +0300

    mety4

--- frontend/src/components/SaryCore.jsx
+++ frontend/src/components/SaryCore.jsx
@@ -78,7 +78,7 @@ const SaryCore = () => {
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
-              alignItems: "center",xcvxcdvd ergergr egergeg
+              alignItems: "center",xcvxcdvd ergergr
             }}
           >
             <img
