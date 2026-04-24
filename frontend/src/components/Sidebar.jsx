5a9341488b7dedb12878d14b98460509387408bd
unknown <mandaniainaprecieux@yahoo.com>
2026-04-24 23:21:23 +0300

    metb

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
           <Navlink
             path={item.path}
             active={item.active}
-            title={item.title}dfgdgdfg dgdfgddf dfgdfgd
+            title={item.title}dfgdgdfg dgdfgddf
             icon={item.icon}
           />
         ))}
