0696d12f931179de058be9a09891261d006c656e
unknown <mandaniainaprecieux@yahoo.com>
2026-04-25 21:45:58 +0300

    m2

--- frontend/src/components/Trajet.jsx
+++ frontend/src/components/Trajet.jsx
@@ -9,7 +9,7 @@ const Trajet = () => {
           {/* sary lalana */}
           <div>
             <img
-              src="../public/images/trajet3.png" sdfsdfsd dsfsdfsdf sdfsdfsdfsdf sdfsdfsdfsdf sdfsdfsdfsdf sdfsdfsdf sdfsdfsdfsd sdfsfsdffsfdsf 
+              src="../public/images/trajet3.png" sdfsdfsd dsfsdfsdf sdfsdfsdfsdf sdfsdfsdfsdf sdfsdfsdfsdf sdfsdfsdf sdfsdfsdfsd sdfsfsdffsfdsf
               className=" pt-20"
               alt=""
               style={{ height: "900px" }}
