934e9bd63ae1f6e749cec358659159e2205c006c
unknown <mandaniainaprecieux@yahoo.com>
2026-04-25 21:51:37 +0300

    m15

--- frontend/src/components/Trajet.jsx
+++ frontend/src/components/Trajet.jsx
@@ -16,7 +16,7 @@ const Trajet = () => {
             />
           </div>
           {/* photo depart */}
-          <div style={{ position: "relative", top: "-900px", left: "65px" }}>fsfsdf sdfsdfsdf sdfsdfsdf sdfsdfsd sdfsdfsf  sdfsdfs sdfsdfs ssdfsdf
+          <div style={{ position: "relative", top: "-900px", left: "65px" }}>fsfsdf sdfsdfsdf sdfsdfsdf sdfsdfsd sdfsdfsf  sdfsdfs sdfsdfs
             <input
               type="text"
               className="bg-gray-200 h-8 rounded-md border-blue-500 text-center"
