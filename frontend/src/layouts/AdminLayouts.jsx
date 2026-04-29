8ef1e8e2d72b38e34367958bc19fa9ccfe468c06
unknown <mandaniainaprecieux@yahoo.com>
2026-04-29 23:17:53 +0300

    o3

--- frontend/src/layouts/AdminLayouts.jsx
+++ frontend/src/layouts/AdminLayouts.jsx
@@ -10,7 +10,7 @@ const AdminLayouts = () => {
       <div className="content  grid  grid-rows-[60px,1fr] md:col-[2]">
         <Navbar />
 
-        <div className="row-[2] dark:bg-slate-950 dark:text-white"> qsdsf fffff qsdfqs sdfsff
+        <div className="row-[2] dark:bg-slate-950 dark:text-white"> qsdsf fffff qsdfqs
           <Outlet />
         </div>
       </div>
