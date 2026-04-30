01d2026c586c76e8b11c59c0687cfb402c4162ca
unknown <mandaniainaprecieux@yahoo.com>
2026-04-30 18:48:13 +0300

    p6

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <main className="relative">
       <Navbar />
-      <div className="mt-20">drrdterte erterterte erterter ertertet ertertet
+      <div className="mt-20">drrdterte erterterte erterter ertertet
         <Outlet />
       </div>
     </main>
