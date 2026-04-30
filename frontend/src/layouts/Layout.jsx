04544f0e172c41888c40700b3c128f073c2fabcb
unknown <mandaniainaprecieux@yahoo.com>
2026-04-30 18:49:01 +0300

    p9

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <main className="relative">
       <Navbar />
-      <div className="mt-20">drrdterte erterterte
+      <div className="mt-20">drrdterte
         <Outlet />
       </div>
     </main>
