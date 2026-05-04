baf2247caabddb0e85d2458d54b89f2981eea38a
unknown <mandaniainaprecieux@yahoo.com>
2026-05-04 23:32:26 +0300

    i6

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
   const { user } = useContext(UserContext);
   return (
     <div>
-      <h1>Dashboard dfd</h1>
+      <h1>Dashboard </h1>
       <>{!!user && <h2>Hi {user.name} !</h2>}</>
     </div>
   );
