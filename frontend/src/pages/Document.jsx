04886a431ba00859d98623bbf765f5c65143e8e2
unknown <mandaniainaprecieux@yahoo.com>
2026-05-05 20:31:02 +0300

    p9

--- frontend/src/pages/Document.jsx
+++ frontend/src/pages/Document.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Document = () => {
   return (
-    <div><div>sdfsdfsd sdfsdfsdfs sdfsdfsdf sdfsdfsdfsdfs sdfsdfsdfs</div>Document</div>
+    <div><div>sdfsdfsd sdfsdfsdfs sdfsdfsdf</div>Document</div>
   )
 }
 
