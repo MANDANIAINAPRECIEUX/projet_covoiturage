3c2de9aba74de71ff0ed1f5d9b9ac8dd864a0e7b
unknown <mandaniainaprecieux@yahoo.com>
2026-05-10 00:20:19 +0300

    b6

--- frontend/src/pages/Reservations.jsx
+++ frontend/src/pages/Reservations.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Reservations = () => {
-  return <div>Reservations  iihuhu uuygyug uvuyvyuv</div>;
+  return <div>Reservations  iihuhu uuygyug</div>;
 };
 
 export default Reservations;
