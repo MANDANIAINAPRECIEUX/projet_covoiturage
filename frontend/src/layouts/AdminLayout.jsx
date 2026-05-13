d8a5d47368c051d9c48ffa69f3f6ac766550a213
unknown <mandaniainaprecieux@yahoo.com>
2026-05-13 23:28:35 +0300

    h2

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AdminLayout = () => {
-  return <div>AdminLayout sdfsdfs sdfsdfsdf sdfsdfsfs sfsdfsfs sdfsdfsfsf</div>;
+  return <div>AdminLayout sdfsdfs sdfsdfsdf sdfsdfsfs sfsdfsfs</div>;
 };
 
 export default AdminLayout;
