c5d735590cb316d92d9c199445d5139948a3cf5c
unknown <mandaniainaprecieux@yahoo.com>
2026-05-15 22:13:31 +0300

    P3

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
     <main className="relative">
       <Navbar />
       <div className="mt-20">
-        <Outlet /> HYHGHGY UUIGUGUIG IGUIGIUGU IUGUIGUIG  
+        <Outlet /> HYHGHGY UUIGUGUIG IGUIGIUGU
       </div>
     </main>
   );
